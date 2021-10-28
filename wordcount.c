e30add5d8e916159e1cdee1b890e0e2a5b9d4a34
william0608 <wnguimfack@gmail.com>
2021-10-28 10:49:10 +0200

    bugfix: Correct some issues after test

--- wordcount.c
+++ wordcount.c
@@ -3,42 +3,8 @@
 
 #define DEF_LINE_LENGTH 1023
 
-
-
-/**!Comment: This function reads input files for standard input
- * Input: standard Input
- * Output: array of file name
- **/
-void func_vidrequestinputfiles(){
-   char buf_localstr[DEF_LINE_LENGTH]; // temp buffer to hold string from standard input
-   char loc_listOffile[DEF_LINE_LENGTH][DEF_LINE_LENGTH]; // temp buffer to hold the word (input file name)
-   int i,j,filecount;
- 
-   printf("please enter the list of files : ");
-   fgets(buf_localstr, sizeof buf_localstr, stdin);   
-
-   /*!Comment: find word from string */
-   j=0; filecount=0;
-   for(i=0;i<=(strlen(buf_localstr));i++){
-      if(buf_localstr[i]==' '|| buf_localstr[i]=='\0'){
-         loc_listOffile[filecount][j]='\0';
-         filecount++;
-         j=0;
-      }
-      else
-      {
-         loc_listOffile[filecount][j]=buf_localstr[i];
-         j++;
-      }
-   }
-
-   /*!Comment: Build the input file*/
-   for(i=0;i < filecount;i++){
-      loc_listOffile[i][strcspn(loc_listOffile[i], "\n")] = 0;
-   }
-
-   return;
-}
+FILE *fp_builtinputfile = NULL;
+char *builtinputfilename = "tmp_finalinputfiles.txt";
 
 
 /**!Comment: This function concatenate the input stream 
@@ -54,8 +20,8 @@ int func_vidbuildinputstream(char *var_inputfile){
    fp_inputfile = fopen(var_inputfile, "r");
    if (fp_inputfile == NULL)
    {
-printf("errr %s",var_inputfile);
-   return 1;
+      printf("errr %s",var_inputfile);
+      return 1;
    } 
    /*!Comment: read fp_inputfile and append the content in fp_builtinputfile */
    while(fgets(char_buffer, DEF_LINE_LENGTH, fp_inputfile)){
@@ -68,6 +34,43 @@ printf("errr %s",var_inputfile);
 }
 
 
+/**!Comment: This function reads input files for standard input
+ * Input: standard Input
+ * Output: array of file name
+ **/
+void func_vidrequestinputfiles(){
+   char buf_localstr[DEF_LINE_LENGTH]; // temp buffer to hold string from standard input
+   char loc_listOffile[DEF_LINE_LENGTH][DEF_LINE_LENGTH]; // temp buffer to hold the word (input file name)
+   int i,j,filecount;
+ 
+   printf("please enter the list of files : ");
+   fgets(buf_localstr, sizeof buf_localstr, stdin);   
+
+   /*!Comment: find word (files names) from string */
+   j=0; filecount=0;
+   for(i=0;i<=(strlen(buf_localstr));i++){
+      if(buf_localstr[i]==' '|| buf_localstr[i]=='\0'){ // check the word ending
+         loc_listOffile[filecount][j]='\0';
+         filecount++; // Increment to word counter
+         j=0;
+      }
+      else
+      {
+         loc_listOffile[filecount][j]=buf_localstr[i];
+         j++;
+      }
+   }
+
+   /*!Comment: Build the input stream*/
+   for(i=0;i < filecount;i++){
+      loc_listOffile[i][strcspn(loc_listOffile[i], "\n")] = 0; // Clean the word
+      func_vidbuildinputstream(loc_listOffile[i]);
+   }
+
+   return;
+}
+
+
 int main(int argc, char **argv)
 {
    int i;
@@ -77,11 +80,16 @@ int main(int argc, char **argv)
       printf("No dictfile found, the program will exit.\n");
       return 0;
    }
+   fp_builtinputfile = fopen(builtinputfilename, "w");
    if (argc == 2){
       func_vidrequestinputfiles();
    }
    if (argc > 2){
+      for (i = 2; i < argc; i++){
+         func_vidbuildinputstream(argv[i]);
+      }
    }
+   fclose(fp_builtinputfile);
 
 
    return 0;
