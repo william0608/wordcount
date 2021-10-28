fc7961abee82d631f843898e33181001fa6de18b
william0608 <wnguimfack@gmail.com>
2021-10-28 11:18:31 +0200

    add the function func_vidParseDictfile() to parse the dictfile and add the current step to the doc

--- wordcount.c
+++ wordcount.c
@@ -4,7 +4,67 @@
 #define DEF_LINE_LENGTH 1023
 
 FILE *fp_builtinputfile = NULL;
+FILE *fp_dictfilestream = NULL;
 char *builtinputfilename = "tmp_finalinputfiles.txt";
+char *builtdictfilename = "tmp_finaldictfile.txt";
+char *dictfilename = "tmp_dictfile.txt";
+
+/*!Comment: This function parses dictfile and removes empty lines and # started lines.
+ * Input: var_dictfilename - dictfile file name (from commad line)
+ * Output: builtdictfilename - file where key from dictfile are saved 
+ * */
+int func_vidParseDictfile(char *var_dictfilename){
+   char char_buffer[DEF_LINE_LENGTH];
+   char loc_StringOfWords[DEF_LINE_LENGTH][DEF_LINE_LENGTH];
+   int loc_wordcount=0; // Index to count the number of word
+   int i; //index to run through characters
+   int j; // index to run through the words
+   FILE *fp_dictfilestreamOut = NULL;
+   FILE *fp_wordtocountstream = NULL;
+
+
+   fp_dictfilestream = fopen(var_dictfilename, "r");
+   fp_dictfilestreamOut = fopen(dictfilename, "w");
+   fp_wordtocountstream = fopen(builtdictfilename, "w");
+   if (fp_dictfilestream == NULL) return 1;
+   if (fp_wordtocountstream == NULL) return 1;
+   if (fp_dictfilestreamOut == NULL) return 1;
+      
+   while(fgets(char_buffer, DEF_LINE_LENGTH, fp_dictfilestream)){
+      if((!(strlen(char_buffer) == 0)) && (char_buffer[0] != '#') && (char_buffer[0] != '\n')){
+         j=0; 
+         loc_wordcount=0;
+         for(i=0;i<=(strlen(char_buffer));i++)
+         {
+            /*!Comment: find word from new line */
+            if(char_buffer[i]==' '||char_buffer[i]=='\0')
+            {
+               loc_StringOfWords[loc_wordcount][j]='\0';
+               loc_wordcount++;
+               j=0;
+            }
+            else
+            {
+               loc_StringOfWords[loc_wordcount][j]=char_buffer[i];
+               j++;
+            }
+         }
+
+         /*!Comment: build the new dictfile and create an input stream from one word line of dictfile */
+         if (loc_wordcount == 1){
+            fprintf(fp_wordtocountstream, "%s", loc_StringOfWords[0]);
+         } else { //TODO delete
+           for(i=0;i < loc_wordcount;i++)
+              fprintf(fp_dictfilestreamOut, "%s", loc_StringOfWords[i]);
+         }
+      }
+   }
+
+   /*!Comment: Free memory */
+   fclose(fp_dictfilestream);
+   //fclose(fp_dictfilestreamOut);
+   fclose(fp_wordtocountstream);
+}
 
 
 /**!Comment: This function concatenate the input stream 
@@ -83,11 +143,13 @@ int main(int argc, char **argv)
    fp_builtinputfile = fopen(builtinputfilename, "w");
    if (argc == 2){
       func_vidrequestinputfiles();
+      func_vidParseDictfile(argv[1]);
    }
    if (argc > 2){
       for (i = 2; i < argc; i++){
          func_vidbuildinputstream(argv[i]);
       }
+      func_vidParseDictfile(argv[1]);
    }
    fclose(fp_builtinputfile);
 
